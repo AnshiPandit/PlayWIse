0dad60473862bb236f25ce0815e2293ee6c84f1b
Anshi <pandit24anshi@gmail.com>
2025-04-06 09:57:07 +0545

    Built a Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
+import { Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button>Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // larger than 1020px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
